da2c1cd3d146e639facd59c3bf99f4af96c853a9
Julian Peeters <julianpeeters@gmail.com>
2024-12-05 00:54:18 -0800

    Add variance on File type param
    Allow subtypes of Statement to count as a Statement

--- modules/core/shared/src/main/scala/dc10/File.scala
+++ modules/core/shared/src/main/scala/dc10/File.scala
@@ -2,7 +2,7 @@ package dc10
 
 import java.nio.file.Path
 
-case class File[A](path: Path, contents: List[A])
+case class File[+A](path: Path, contents: List[A])
 object File:
 
   extension [A] (file: File[A])
