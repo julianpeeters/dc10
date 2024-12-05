9daa3a5d45393fc73c2926cffda26bb716814e07
Julian Peeters <julianpeeters@gmail.com>
2024-12-05 00:56:03 -0800

    Bubble up errors

--- modules/io/jvm/src/main/scala/dc10/io/package.scala
+++ modules/io/jvm/src/main/scala/dc10/io/package.scala
@@ -6,8 +6,10 @@ import dc10.{Compiler, File, Renderer}
 import fs2.io.file.{Files, Path}
 
 extension [F[_]: Concurrent: Files, C, D, E, V](
-  res: Either[List[E], List[File[C]]]
+  res: scala.Either[List[E], List[File[C]]]
 )(using C: Compiler[C, D, E])
   def file(using R: Renderer[C, E, V]): F[List[Path]] =
-    C.virtualFile(res)
-      .foldMapM(l => l.traverse(f => FileWriter[F].writeFile(f)))
+    C.virtualFile(res)(using R).fold(
+      es => Concurrent[F].raiseError(new Throwable(es.mkString("\n"))),
+      l => l.traverse(f => FileWriter[F].writeFile(f))
+    )
